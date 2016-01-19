608f4f6c42d8b2deb368aef2899817df7eca82b2
ruanjue <ruanjue@gmail.com>
2016-01-19 21:05:01 +0800

    use DMO as the default overlap-engine

--- smartdenovo.pl
+++ smartdenovo.pl
@@ -4,50 +4,73 @@ use strict;
 use warnings;
 use Getopt::Std;
 
-my %opts = (t=>8, k=>17, m=>0.6, s=>200, J=>0, p=>'wtasm');
+my %opts = (t=>8, e=>'dmo', k=>16, J=>5000, p=>'wtasm', c=>0);
 getopts('t:p:k:m:s:J:', \%opts);
+$opts{e} = lc $opts{e};
 die (qq/Usage: smartdenovo.pl [options] <reads.fa>
 Options:
   -p STR     output prefix [$opts{p}]
+  -e STR     engine of overlaper, compressed kmer overlapper(zmo), dot matrix overlapper(dmo), [$opts{e}]
   -t INT     number of threads [$opts{t}]
   -k INT     k-mer length for overlapping [$opts{k}]
+             for large genome as human, please use 17
   -J INT     min read length [$opts{J}]
-  -m FLOAT   min identity [$opts{m}]
-  -s INT     min alignment score [$opts{s}]
+  -c INT     generate consensus, [$opts{c}]
 /) if (@ARGV == 0);
+
+die("Engine choice: dmo, zmo\n") if($opts{e} ne 'dmo' and $opts{e} ne 'zmo');
 
 $opts{pre} = gwhich("wtpre") || die;
 $opts{zmo} = gwhich("wtzmo") || die;
+$opts{obt} = gwhich("wtobt") || die;
 $opts{gbo} = gwhich("wtgbo") || die;
 $opts{clp} = gwhich("wtclp") || die;
 $opts{lay} = gwhich("wtlay") || die;
 $opts{cns} = gwhich("wtcns") || die;
 
+
 my @lines = ();
 push(@lines, qq/PREFIX=$opts{p}/, '');
-push(@lines, qq/EXE_PRE=$opts{pre}/, qq/EXE_ZMO=$opts{zmo}/, qq/EXE_GBO=$opts{gbo}/, qq/EXE_CLP=$opts{clp}/, qq/EXE_LAY=$opts{lay}/, qq/EXE_CNS=$opts{cns}/);
+push(@lines, qq/EXE_PRE=$opts{pre}/, qq/EXE_ZMO=$opts{zmo}/, qq/EXE_OBT=$opts{obt}/, qq/EXE_GBO=$opts{gbo}/, qq/EXE_CLP=$opts{clp}/, qq/EXE_LAY=$opts{lay}/, qq/EXE_CNS=$opts{cns}/);
 push(@lines, qq/N_THREADS=$opts{t}/, "");
 
-push(@lines, q/all:$(PREFIX).cns/, "");
+if($opts{c}){
+	push(@lines, qq/all:\$(PREFIX).$opts{e}.cns/, "");
+} else {
+	push(@lines, qq/all:\$(PREFIX).$opts{e}.lay/, "");
+}
 
-push(@lines, q/$(PREFIX).pre.gz:/);
-push(@lines, qq/\t\$(EXE_PRE) -L -J $opts{J} $ARGV[0] 2> \$\@.log | gzip -1 > \$\@/, "");
+push(@lines, q/$(PREFIX).fa.gz:/);
+push(@lines, qq/\t\$(EXE_PRE) -J $opts{J} $ARGV[0] | gzip -c -1 > \$\@/, "");
 
-push(@lines, q/$(PREFIX).ovl.short:$(PREFIX).pre.gz/);
-push(@lines, qq/\t\$(EXE_ZMO) -t \$(N_THREADS) -i \$(PREFIX).pre.gz -fo - -k $opts{k} -s $opts{s} -m $opts{m} 2> \$\@.log | cut -f1-16 > \$\@/, "");
+if($opts{e} eq 'dmo'){
+	push(@lines, q/$(PREFIX).dmo.ovl:$(PREFIX).fa.gz/);
+	push(@lines, qq/\t\$(EXE_ZMO) -t \$(N_THREADS) -i \$(PREFIX).fa.gz -fo \$\@ -k $opts{k} -z 10 -Z 16 -U -1 -m 0.1/, "");
 
-push(@lines, q/$(PREFIX).gbo.short:$(PREFIX).pre.gz $(PREFIX).ovl.short/);
-push(@lines, qq/\t\$(EXE_GBO) -t \$(N_THREADS) -i \$< -j \$(PREFIX).ovl.short -fo - 2> \$\@.log | cut -f1-16 > \$\@/, "");
+	push(@lines, q/$(PREFIX).dmo.obt:$(PREFIX).fa.gz $(PREFIX).dmo.ovl/);
+	push(@lines, qq/\t\$(EXE_CLP) -i \$(PREFIX).dmo.ovl -fo \$\@ -d 3 -k 300 -m 0.1 -FT/, "");
 
-push(@lines, q/$(PREFIX).obt:$(PREFIX).ovl.short $(PREFIX).gbo.short/);
-push(@lines, qq/\t\$(EXE_CLP) -i \$< -i \$(PREFIX).gbo.short -fo \$\@ -F -d 2 2> \$\@.log/, "");
+	push(@lines, q/$(PREFIX).dmo.lay:$(PREFIX).fa.gz $(PREFIX).dmo.obt $(PREFIX).dmo.ovl/);
+	push(@lines, qq/\t\$(EXE_LAY) -i \$(PREFIX).fa.gz -b \$(PREFIX).dmo.obt -j \$(PREFIX).dmo.ovl -fo \$(PREFIX).dmo.lay -w 300 -s 200 -m 0.1 -r 0.95 -c 1/, "");
 
-push(@lines, q/$(PREFIX).lay.utg $(PREFIX).lay:$(PREFIX).pre.gz $(PREFIX).ovl.short $(PREFIX).gbo.short $(PREFIX).obt/);
-push(@lines, qq/\t\$(EXE_LAY) -i \$< -b \$(PREFIX).obt -j \$(PREFIX).ovl.short -j \$(PREFIX).gbo.short -fo \$(PREFIX).lay -s $opts{s} -m $opts{m} -R -r 1 -c 1 2> \$(PREFIX).lay.log/, "");
+} else {
+	push(@lines, q/$(PREFIX).zmo.ovl.short:$(PREFIX).fa.gz/);
+	push(@lines, qq/\t\$(EXE_ZMO) -t \$(N_THREADS) -i \$(PREFIX).fa.gz -fo - -k $opts{k} -s 200 -m 0.6 | cut -f1-16 > \$\@/, "");
 
-push(@lines, q/$(PREFIX).cns:$(PREFIX).lay/);
-push(@lines, qq/\t\$(EXE_CNS) -t \$(N_THREADS) \$< > \$\@ 2> \$\@.log/, "");
+	push(@lines, q/$(PREFIX).zmo.gbo.short:$(PREFIX).fa.gz $(PREFIX).zmo.ovl.short/);
+	push(@lines, qq/\t\$(EXE_GBO) -t \$(N_THREADS) -i \$< -j \$(PREFIX).zmo.ovl.short -fo - | cut -f1-16 > \$\@/, "");
 
+	push(@lines, q/$(PREFIX).zmo.obt:$(PREFIX).zmo.ovl.short $(PREFIX).zmo.gbo.short/);
+	push(@lines, qq/\t\$(EXE_CLP) -i \$< -i \$(PREFIX).zmo.gbo.short -fo \$\@ -F -d 2/, "");
+
+	push(@lines, q/$(PREFIX).zmo.lay:$(PREFIX).fa.gz $(PREFIX).zmo.ovl.short $(PREFIX).zmo.gbo.short $(PREFIX).zmo.obt/);
+	push(@lines, qq/\t\$(EXE_LAY) -i \$< -b \$(PREFIX).zmo.obt -j \$(PREFIX).zmo.ovl.short -j \$(PREFIX).zmo.gbo.short -fo \$(PREFIX).zmo.lay -s 200 -m 0.6 -R -r 1 -c 1/, "");
+}
+
+if($opts{c}){
+	push(@lines, qq/\$(PREFIX).$opts{e}.cns:\$(PREFIX).$opts{e}.lay/);
+	push(@lines, qq/\t\$(EXE_CNS) -t \$(N_THREADS) \$< > \$\@ 2> \$\@.log/, "");
+}
 print(join("\n", @lines), "\n");
 
 sub which {
