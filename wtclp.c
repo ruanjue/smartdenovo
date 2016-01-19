6c3812aa0721f71b37628dab8bd717d94429fcdb
ruanjue <ruanjue@gmail.com>
2016-01-19 20:37:02 +0800

    add new module for DMO, use -T

--- wtclp.c
+++ wtclp.c
@@ -24,6 +24,12 @@
 #include "timer.h"
 #include "file_reader.h"
 
+#define CHIMERA_FREE_TAIL_N_BIN	0
+
+static char *spec_read_debug = NULL;
+
+static int clp_debug = 0;
+
 typedef struct {
 	uint64_t x:20, y:20, dir:1, legal:1, aux:22;
 } pb_frg_t;
@@ -50,6 +56,11 @@ typedef struct {
 	uint32_t inc;
 } pb_evt_t;
 define_list(pbevtv, pb_evt_t);
+
+typedef struct {
+	int pos, dir, spur, dep;
+} spur_t;
+define_list(spurv, spur_t);
 
 typedef struct {
 	pbseqv   *seqs;
@@ -221,7 +232,7 @@ uint64_t call_legal_overlaps_wtclp(WTCLP *wt){
 	return ret;
 }
 
-void clp_high_err_region_wtclp(WTCLP *wt, uint32_t min_dep){
+void clp_high_err_region_wtclp(WTCLP *wt, uint32_t min_dep, int whole){
 	pb_seq_t *pb;
 	pb_aln_t *hit;
 	u4v *brks;
@@ -245,13 +256,16 @@ void clp_high_err_region_wtclp(WTCLP *wt, uint32_t min_dep){
 			push_u4v(brks, ((hit->pair[k].y << 1)) | 1);
 		}
 		if(brks->size == 0){
-			pb->clp_x = pb->clp_y = 0; pb->closed = 2;
+			pb->clp_x = pb->clp_y = 0; pb->closed = 3;
 		} else {
 			sort_array(brks->buffer, brks->size, uint32_t, (a >> 1) > (b >> 1));
 			if(fix){
 				pb->fix = 1;
-				pb->clp_x = brks->buffer[0] >> 1;
-				pb->clp_y = brks->buffer[brks->size - 1] >> 1;
+				if(whole){
+				} else {
+					pb->clp_x = brks->buffer[0] >> 1;
+					pb->clp_y = brks->buffer[brks->size - 1] >> 1;
+				}
 			} else {
 				dep = max = xx = yy = mx = my = 0;
 				for(j=0;j<brks->size;j++){
@@ -270,8 +284,10 @@ void clp_high_err_region_wtclp(WTCLP *wt, uint32_t min_dep){
 						if(dep == min_dep) xx = pos;
 					}
 				}
-				if(mx + pb->len > my + wt->bin_size){
-					pb->clp_x = pb->clp_y = 0; pb->closed = 3;
+				if(whole){
+					if(mx > wt->bin_size || pb->len - my > wt->bin_size){
+						pb->clp_x = pb->clp_y = 0; pb->closed = 3;
+					}
 				} else {
 					pb->clp_x = mx;
 					pb->clp_y = my;
@@ -281,11 +297,6 @@ void clp_high_err_region_wtclp(WTCLP *wt, uint32_t min_dep){
 	}
 	free_u4v(brks);
 }
-
-typedef struct {
-	int pos, dir, spur, dep;
-} spur_t;
-define_list(spurv, spur_t);
 
 int detect_chimera_wtclp(WTCLP *wt, uint32_t sid, spurv *crss, spurv *chis, int win_size, int min_crs_dep){
 	pb_seq_t *pb1, *pb2;
@@ -403,6 +414,312 @@ uint32_t filter_chimeric_seqs_wtclp(WTCLP *wt, int win_size, int min_crs_dep, in
 	return ret;
 }
 
+int distinguish_chimera_wtclp(WTCLP *wt, uint32_t sid, spurv *fine,  spurv *crss, spurv *chis, int min_crs_dep){
+	pb_seq_t *pb1, *pb2;
+	pb_aln_t *hit;
+	spur_t *s, *t, S;
+	uint64_t idx;
+	uint32_t i, j, m, k, mx;
+	int x, y, max, d[4];
+	if(min_crs_dep == 0) return 0;
+	pb1 = ref_pbseqv(wt->seqs, sid);
+	if(pb1->closed) return 0;
+	if(pb1->fix) return 0;
+	if(pb1->clp_x >= pb1->clp_y) return 0;
+	memset(&S, 0, sizeof(spur_t));
+	S.pos = 0x7FFFFFFF;
+	clear_spurv(fine);
+	clear_spurv(crss);
+	for(i=0;i<pb1->alncnt;i++){
+		idx = wt->ptrs->buffer[pb1->alnoff + i];
+		k = idx & 0x01;
+		idx >>= 1;
+		hit = ref_pbalnv(wt->hits, idx);
+		pb2 = ref_pbseqv(wt->seqs, hit->sids[!k]);
+		//if(pb2->closed) continue;
+		d[0] = (int)hit->pair[k].x - (int)pb1->clp_x;
+		d[1] = (int)pb1->clp_y - (int)hit->pair[k].y;
+		d[2] = (int)hit->pair[!k].x - (int)pb2->clp_x;
+		d[3] = (int)pb2->clp_y - (int)hit->pair[!k].y;
+		if(hit->pair[0].dir ^ hit->pair[1].dir){
+			d[2] = d[2] ^ d[3]; d[3] = d[2] ^ d[3]; d[2] = d[2] ^ d[3]; // swap
+		}
+		x = 0;
+		if(d[0] > (int)wt->bin_size){
+			if(d[2] > (int)wt->bin_size){ // x spur
+				push_spurv(crss, (spur_t){hit->pair[k].x / wt->bin_size, 0, num_min(hit->pair[k].y, pb1->clp_y) / wt->bin_size, 0});
+				x = 1;
+			}
+		}
+		 y = 0;
+		if(d[1] > (int)wt->bin_size){
+			if(d[3] > (int)wt->bin_size){
+				push_spurv(crss, (spur_t){hit->pair[k].y / wt->bin_size, 1, num_max(hit->pair[k].x, pb1->clp_x) / wt->bin_size, 0});
+				y = 1;
+			}
+		}
+		if(x == 0 && y == 0) push_spurv(fine, (spur_t){hit->pair[k].x / wt->bin_size, 3, hit->pair[k].y / wt->bin_size, 1});
+	}
+	if((int)crss->size < min_crs_dep) return 0;
+	sort_array(crss->buffer, crss->size, spur_t, num_cmpgtx(a.pos, b.pos, a.dir, b.dir));
+	clear_spurv(chis);
+	for(j=0,i=1;i<=crss->size;i++){
+		s = (i == crss->size)? &S : ref_spurv(crss, i);
+		if(s->pos == crss->buffer[j].pos && s->dir == crss->buffer[j].dir) continue;
+		//if(i - j >= (uint32_t)min_crs_dep && crss->buffer[j].pos > CHIMERA_FREE_TAIL_N_BIN && crss->buffer[j].pos + CHIMERA_FREE_TAIL_N_BIN < (int)(pb1->clp_y / wt->bin_size)){
+		if(i - j >= (uint32_t)min_crs_dep){
+			t = next_ref_spurv(chis);
+			sort_array(crss->buffer + j, i - j, spur_t, a.spur > b.spur);
+			s = ref_spurv(crss, j);
+			t->pos = s->pos;
+			t->dir = s->dir;
+			t->dep = i - j;
+			t->spur = s->pos;
+			if(clp_debug){
+				fprintf(stderr, "DETECTED [%s] pos={%d,%d} dir=%c n_spur=%d\n", pb1->tag, t->pos * wt->bin_size, t->spur * wt->bin_size, "+-"[t->dir], t->dep);
+			}
+			if(clp_debug){
+				for(m=j;m<i;m++){
+					s = ref_spurv(crss, m);
+					fprintf(stderr, "Quote pos={%d,%d}\n", s->pos * wt->bin_size, s->spur * wt->bin_size);
+				}
+			}
+			if(t->dir){
+				t->spur = crss->buffer[j + min_crs_dep].spur;
+			} else {
+				t->spur = crss->buffer[i - min_crs_dep].spur;
+			}
+		}
+		j = i;
+	}
+	if(chis->size == 0) return 0;
+	// check whether segments are syntenic, also coverage
+	clear_spurv(crss);
+	for(i=0;i<chis->size;i++){
+		s = ref_spurv(chis, i);
+		if(clp_debug){
+			fprintf(stderr, "SOLVING [%s] pos={%d,%d} dir=%c n_spur=%d\n", pb1->tag, s->pos * wt->bin_size, s->spur * wt->bin_size, "+-"[s->dir], s->dep);
+		}
+		s->dep = 0;
+		s->dep ++; // pb1 support once
+		for(j=0;j<fine->size;j++){
+			t = ref_spurv(fine, j);
+			if(s->dir){
+				if(t->pos <= s->spur && t->spur > s->pos){
+					s->dep ++;
+					if(clp_debug){
+						fprintf(stderr, "Plea pos={%d,%d}\n", t->pos * wt->bin_size, t->spur * wt->bin_size);
+					}
+				}
+			} else {
+				if(t->pos < s->pos && t->spur >= s->spur){
+					s->dep ++;
+					if(clp_debug){
+						fprintf(stderr, "Plea pos={%d,%d}\n", t->pos * wt->bin_size, t->spur * wt->bin_size);
+					}
+				}
+			}
+		}
+		if(s->dep < min_crs_dep) push_spurv(crss, *s);
+	}
+	if(crss->size == 0) return 0;
+	sort_array(crss->buffer, crss->size, spur_t, num_cmpgt(a.pos, b.pos));
+	x = 0; y = 0; mx = 0; max = 0;
+	for(i=0;i<crss->size;i++){
+		s = ref_spurv(crss, i);
+		if(s->pos - x > max){
+			mx = i; max = s->pos - x; y = x;
+		}
+		x = s->pos;
+	}
+	if(pb1->clp_y - x * wt->bin_size > (max? max : 1) * wt->bin_size){
+		y = pb1->clp_y / wt->bin_size;
+	} else {
+		x = y;
+		y = ref_spurv(crss, mx)->pos;
+	}
+	pb1->clp_x = x * wt->bin_size;
+	pb1->clp_y = y * wt->bin_size;
+	return 1;
+}
+
+uint32_t distinguish_chimeric_seqs_wtclp(WTCLP *wt, int min_crs_dep, int whole){
+	spurv *fine, *crss, *chis;
+	uint32_t i, ret;
+	ret = 0;
+	fine = init_spurv(1024);
+	crss = init_spurv(1024);
+	chis = init_spurv(1024);
+	for(i=0;i<wt->seqs->size;i++){
+		if(wt->seqs->buffer[i].closed == 0 && distinguish_chimera_wtclp(wt, i, fine, crss, chis, min_crs_dep)){
+			if(whole) wt->seqs->buffer[i].closed = 1;
+			ret ++;
+		}
+	}
+	free_spurv(fine);
+	free_spurv(crss);
+	free_spurv(chis);
+	return ret;
+}
+
+int test_chimera_wtclp(WTCLP *wt, uint32_t sid, spurv *fine,  spurv *crss, spurv *chis, int min_crs_dep){
+	pb_seq_t *pb1, *pb2;
+	pb_aln_t *hit;
+	spur_t *s, *t, S;
+	uint64_t idx;
+	uint32_t i, j, m, k, mx, my;
+	int x, y, len, max, d[4], ret;
+	if(min_crs_dep == 0) return 0;
+	pb1 = ref_pbseqv(wt->seqs, sid);
+	if(pb1->closed) return 0;
+	//if(pb1->fix) return 0;
+	if(pb1->clp_x >= pb1->clp_y) return 0;
+	memset(&S, 0, sizeof(spur_t));
+	S.pos = 0x7FFFFFFF;
+	clear_spurv(fine);
+	clear_spurv(crss);
+	if(spec_read_debug) clp_debug = !strcmp(pb1->tag, spec_read_debug);
+	for(i=0;i<pb1->alncnt;i++){
+		idx = wt->ptrs->buffer[pb1->alnoff + i];
+		k = idx & 0x01;
+		idx >>= 1;
+		hit = ref_pbalnv(wt->hits, idx);
+		pb2 = ref_pbseqv(wt->seqs, hit->sids[!k]);
+		//if(pb2->closed) continue;
+		//d[0] = (int)hit->pair[k].x - (int)pb1->clp_x;
+		//d[1] = (int)pb1->clp_y - (int)hit->pair[k].y;
+		//d[2] = (int)hit->pair[!k].x - (int)pb2->clp_x;
+		//d[3] = (int)pb2->clp_y - (int)hit->pair[!k].y;
+		d[0] = (int)hit->pair[k].x - (int)pb1->clp_x;
+		d[1] = (int)pb1->clp_y - (int)hit->pair[k].y;
+		d[2] = (int)hit->pair[!k].x - (int)0;
+		d[3] = (int)pb2->len - (int)hit->pair[!k].y;
+		if(hit->pair[0].dir ^ hit->pair[1].dir){
+			d[2] = d[2] ^ d[3]; d[3] = d[2] ^ d[3]; d[2] = d[2] ^ d[3]; // swap
+		}
+		x = 0;
+		if(d[0] > (int)wt->bin_size){
+			if(d[2] > (int)wt->bin_size){ // x spur
+				push_spurv(crss, (spur_t){hit->pair[k].x / wt->bin_size, 0, num_min(hit->pair[k].y, pb1->clp_y) / wt->bin_size, 0});
+				x = 1;
+			}
+		}
+		 y = 0;
+		if(d[1] > (int)wt->bin_size){
+			if(d[3] > (int)wt->bin_size){
+				push_spurv(crss, (spur_t){hit->pair[k].y / wt->bin_size, 1, num_max(hit->pair[k].x, pb1->clp_x) / wt->bin_size, 0});
+				y = 1;
+			}
+		}
+		if(x == 0 && y == 0) push_spurv(fine, (spur_t){hit->pair[k].x / wt->bin_size, 3, hit->pair[k].y / wt->bin_size, 1});
+	}
+	if((int)crss->size < min_crs_dep) return 0;
+	if(clp_debug){
+		fprintf(stderr, "[%s] Total overlaps:%d\n", pb1->tag, (int)pb1->alncnt);
+		fprintf(stderr, "[%s] Fine  overlaps:%d\n", pb1->tag, (int)fine->size);
+		fprintf(stderr, "[%s] potential spur:%d\n", pb1->tag, (int)crss->size);
+	}
+	sort_array(crss->buffer, crss->size, spur_t, num_cmpgt(a.pos, b.pos));
+	clear_spurv(chis);
+	for(j=0,i=1;i<=crss->size;i++){
+		s = (i == crss->size)? &S : ref_spurv(crss, i);
+		if(s->pos == crss->buffer[j].pos) continue;
+		if(i - j >= (uint32_t)min_crs_dep && crss->buffer[j].pos > CHIMERA_FREE_TAIL_N_BIN && crss->buffer[j].pos + CHIMERA_FREE_TAIL_N_BIN < (int)(pb1->clp_y / wt->bin_size)){
+			t = next_ref_spurv(chis);
+			s = ref_spurv(crss, j);
+			t->pos = s->pos;
+			t->dir = 0;
+			t->dep = i - j;
+			t->spur = s->pos;
+			if(clp_debug){
+				fprintf(stderr, "DETECTED [%s] pos={%d,%d} dir=%c n_spur=%d\n", pb1->tag, t->pos * wt->bin_size, t->spur * wt->bin_size, "+-"[t->dir], t->dep);
+			}
+			if(0 && clp_debug){
+				for(m=j;m<i;m++){
+					s = ref_spurv(crss, m);
+					fprintf(stderr, "Quote pos={%d,%d}\n", s->pos * wt->bin_size, s->spur * wt->bin_size);
+				}
+			}
+		}
+		j = i;
+	}
+	if(chis->size == 0) return 0;
+	// check whether segments are syntenic, also coverage
+	clear_spurv(crss);
+	for(i=0;i<fine->size;i++){
+		s = ref_spurv(fine, i);
+		x = -1; y = 0;
+		for(j=0;j<chis->size;j++){
+			t = ref_spurv(chis, j);
+			if(!(s->pos < t->pos && s->spur > t->pos)) continue;
+			if(x == -1) x = j;
+			y = j;
+		}
+		if(x >= 0){
+			push_spurv(crss, (spur_t){x, 0, y, i});
+		}
+	}
+	ret = 1;
+	mx = 0; my = 0; max = -1;
+	if(crss->size){
+		sort_array(crss->buffer, crss->size, spur_t, num_cmpgtx(a.pos, b.pos, a.spur, b.spur));
+		for(j=i=0;i<=crss->size;i++){
+			s = (i == crss->size)? &S : ref_spurv(crss, i);
+			if(s->pos == crss->buffer[j].pos && s->spur == crss->buffer[j].spur) continue;
+			if(j + min_crs_dep <= i){
+				s = ref_spurv(crss, j);
+				x = s->pos? chis->buffer[s->pos-1].pos * wt->bin_size : pb1->clp_x;
+				y = (s->spur + 1 >= (int)chis->size)? pb1->clp_y : chis->buffer[s->spur + 1].pos * wt->bin_size;
+				len = y - x;
+				if(clp_debug){
+					fprintf(stderr, "Plea %d\t%d,%d\t%d,%d\tdep=%d\n", len, x, y, s->pos, s->spur, i - j);
+				}
+				if(len > max){
+					if(s->pos == 0 && s->spur + 1 == (int)chis->size) ret = 0;
+					max = len; mx = x; my = y;
+				}
+			}
+			j = i;
+		}
+	}
+	if(max == -1){
+		x = num_max(chis->buffer[0].pos * wt->bin_size, pb1->clp_x);
+		y = num_min(chis->buffer[chis->size-1].pos * wt->bin_size, pb1->clp_y);
+		if(x >= pb1->clp_y - y){
+			pb1->clp_y = x;
+		} else {
+			pb1->clp_x = y;
+		}
+		max = y - x;
+	} else {
+		pb1->clp_x = mx;
+		pb1->clp_y = my;
+	}
+	if(clp_debug){
+		fprintf(stderr, "clp: %d\t%d,%d\n", max, pb1->clp_x, pb1->clp_y);
+	}
+	return ret;
+}
+
+uint32_t test_chimeric_seqs_wtclp(WTCLP *wt, int min_crs_dep, int whole){
+	spurv *fine, *crss, *chis;
+	uint32_t i, ret;
+	ret = 0;
+	fine = init_spurv(1024);
+	crss = init_spurv(1024);
+	chis = init_spurv(1024);
+	for(i=0;i<wt->seqs->size;i++){
+		if(wt->seqs->buffer[i].closed == 0 && test_chimera_wtclp(wt, i, fine, crss, chis, min_crs_dep)){
+			if(whole) wt->seqs->buffer[i].closed = 1;
+			ret ++;
+		}
+	}
+	free_spurv(fine);
+	free_spurv(crss);
+	free_spurv(chis);
+	return ret;
+}
+
 int has_alt_path_wtclp(WTCLP *wt, uint32_t sid, UUhash *hash, u8v *stack){
 	pb_seq_t *pb;
 	pb_aln_t *hit, *h1, *h2;
@@ -490,7 +807,7 @@ uint32_t filter_lonely_seqs_wtclp(WTCLP *wt){
 	ret = 0;
 	for(i=0;i<wt->seqs->size;i++){
 		if(wt->seqs->buffer[i].closed == 0 && has_alt_path_wtclp(wt, i, hash, stack) == 0){
-			wt->seqs->buffer[i].closed = 1;
+			wt->seqs->buffer[i].closed = 2;
 			ret ++;
 		}
 	}
@@ -611,13 +928,17 @@ int usage(){
 	" -m <float>  Minimum identity of alignment, [0.6]\n"
 	" -C          Trun off specical reservation for reads contained by others\n"
 	"             Default: one read (A) will not be trimmed when it is contained by another read (B).\n"
-	" -c <int>    Minimum depth of overlap along read, [2]\n"
+	//" -c <int>    Minimum depth of overlap along read, [2]\n"
 	//" -r <int>    Max margin size of legal overlap, [200]\n"
 	" -k <int>    Bin size, [50]\n"
 	" -w <int>    Window size used in chimera detection, [1000]\n"
 	" -d <int>    Min number of solid overlaps in a suspecting region to reject chimeric, [3]\n"
 	" -n <int>    Max turns of iterations, [5]\n"
-	" -x <int>    For debug. 1: chimera checking; 2:conntection checking, [3]\n"
+	" -T          Treat read as a path of many blocks broken by possible chimeric sites, and test whether the path is valid\n"
+	"             will disable iteration, connection checking\n"
+	" -x <int>    For debug. 1: chimera checking; 2: conntection checking; 4: clip high error ending [7]\n"
+	" -v          Verbose\n"
+	" -8 <string> Print message for special read\n"
 	);
 	return 1;
 }
@@ -629,7 +950,7 @@ int main(int argc, char **argv){
 	char *outf;
 	FILE *out;
 	int c, iter, max_iter, force, min_alen, min_dep, bin_size, fix_contained, whole;
-	int win_size, min_crs_dep, debug_x;
+	int win_size, min_crs_dep, block_test, debug_x;
 	float min_sm;
 	uint64_t tol;
 	uint32_t nflt, nclp;
@@ -639,15 +960,15 @@ int main(int argc, char **argv){
 	force = 0;
 	min_alen = 1000;
 	min_sm = 0.6;
-	min_dep = 2;
 	win_size = 1000;
 	min_crs_dep = 3;
 	bin_size = 50;
 	max_iter = 5;
 	fix_contained = 1;
 	whole = 0;
-	debug_x = 3;
-	while((c = getopt(argc, argv, "hi:b:o:fFs:m:Cc:r:w:k:d:n:x:")) != -1){
+	block_test = 0;
+	debug_x = 7;
+	while((c = getopt(argc, argv, "hi:b:o:fFs:m:Cc:r:w:k:d:n:Tx:v8:")) != -1){
 		switch(c){
 			case 'h': return usage();
 			case 'f': force = 1; break;
@@ -664,7 +985,10 @@ int main(int argc, char **argv){
 			case 'd': min_crs_dep = atoi(optarg); break;
 			case 'k': bin_size = atoi(optarg); break;
 			case 'n': max_iter = atoi(optarg); break;
+			case 'T': max_iter = 1; block_test = 1; break;
 			case 'x': debug_x = atoi(optarg); break;
+			case 'v': clp_debug = 1; break;
+			case '8': spec_read_debug = optarg; break;
 			default: return usage();
 		}
 	}
@@ -694,21 +1018,23 @@ int main(int argc, char **argv){
 	}
 	fprintf(stderr, "[%s] clipping based on overlap depth\n", date());
 	tol = call_legal_overlaps_wtclp(wt);
-	if(whole){
-		fprintf(stderr, "Orignal: legal overlaps = %llu\n", (unsigned long long)tol);
-	} else {
-		fprintf(stderr, "Before: legal overlaps = %llu\n", (unsigned long long)tol);
-		clp_high_err_region_wtclp(wt, min_dep);
-		tol = call_legal_overlaps_wtclp(wt);
-		fprintf(stderr, "After:  legal overlaps = %llu\n", (unsigned long long)tol);
-	}
+	fprintf(stderr, "Before: legal overlaps = %llu\n", (unsigned long long)tol);
+	if(debug_x & 0x04) clp_high_err_region_wtclp(wt, min_crs_dep, whole);
+	tol = call_legal_overlaps_wtclp(wt);
+	fprintf(stderr, "After:  legal overlaps = %llu\n", (unsigned long long)tol);
 	fprintf(stderr, "[%s] Done\n", date());
-	fprintf(stderr, "[%s] init iteration\n", date());
 	for(iter=0;iter<max_iter;iter++){
-		fprintf(stderr, "[%s] iteration %d\n", date(), iter + 1);
-		nflt = (debug_x & 0x02)? filter_lonely_seqs_wtclp(wt) : 0;
-		fprintf(stderr, "%u reads were filtered by connection-checking\n", nflt);
-		nclp = (debug_x & 0x01)? filter_chimeric_seqs_wtclp(wt, win_size, min_crs_dep, whole) : 0;
+		if(block_test){
+			nflt = (debug_x & 0x02)? filter_lonely_seqs_wtclp(wt) : 0;
+			fprintf(stderr, "%u reads were filtered by connection-checking\n", nflt);
+			nclp = (debug_x & 0x01)? test_chimeric_seqs_wtclp(wt, min_crs_dep, whole) : 0;
+		} else {
+			fprintf(stderr, "[%s] iteration %d\n", date(), iter + 1);
+			nflt = (debug_x & 0x02)? filter_lonely_seqs_wtclp(wt) : 0;
+			fprintf(stderr, "%u reads were filtered by connection-checking\n", nflt);
+			nclp = (debug_x & 0x01)? filter_chimeric_seqs_wtclp(wt, win_size, min_crs_dep, whole) : 0;
+			//nclp = (debug_x & 0x01)? distinguish_chimeric_seqs_wtclp(wt, min_crs_dep, whole) : 0;
+		}
 		fprintf(stderr, "%u reads were truncated by chimera-checking\n", nclp);
 		tol = call_legal_overlaps_wtclp(wt);
 		fprintf(stderr, "legal overlaps = %llu\n", (unsigned long long)tol);
